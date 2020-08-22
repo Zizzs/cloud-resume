c742bbd443361da2ea854bbca6c5954a7a0493ef
Alex Williams <Zizzs17@gmail.com>
2020-08-22 13:47:54 -0700

    Content added to Certifications, Projects and Skills
    All content has been added for the main pages of the website.

--- src/Components/Certifications.js
+++ src/Components/Certifications.js
@@ -1,9 +1,33 @@
 import React from "react";
+import CCP from "../assets/Images/AWS-CloudPractitioner.png";
 
 function Certifications() {
   return (
-    <div>
-      <p className="text-main">Certifications</p>
+    <div className="bg-mainSoftGray">
+      <p className="text-mainBlack text-left text-4xl sm:text-6xl">
+        Alex Williams
+      </p>
+
+      <div className="w-full mt-12 text-white bg-mainGunmetal border-mainDarkBlue border-4">
+        <div className="p-5">
+          <div>
+            <p className="text-lg sm:text-3xl lg:text-3xl">Certifications</p>
+          </div>
+          <hr />
+          <div className="p-2 sm:p-10 xl:grid xl:grid-cols-2">
+            <div>
+              <img
+                className="xl:w-2/4 mx-auto"
+                src={CCP}
+                alt="Certified Cloud Practitioner Badge"
+              />
+              <p className="text-center sm:text-3xl xl:text-base">
+                Validation Number: 15ER3B9CN2R1QEGB
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,9 +1,52 @@
 import React from "react";
 
 function Projects() {
+  const projects = [
+    {
+      title: "Cloud Resume",
+      description: "This website, a AWS cloud hosted portfolio and resume.",
+      link: "https://github.com/Zizzs/cloud-resume",
+    },
+    {
+      title: "Void, MERN Stack PBBG",
+      description:
+        "Void is an in development Persistant Browser Based Game focusing on RPG style mechanics set in a multi-dimensional space themed world.",
+      link: "https://github.com/Zizzs/MERN-Stack-RPG",
+    },
+  ];
+
+  const projectsDiv = projects.map((index) => (
+    <div>
+      <br />
+      <p className="text-xl sm:text-5xl xl:text-3xl">{index.title}</p>
+      <p className="text-base sm:text-3xl xl:text-base">{index.description}</p>
+      <p>
+        <a
+          className="text-blue-300 hover:text-blue-200 text-base sm:text-2xl xl:text-base"
+          href={index.link}
+        >
+          Github Repo Link
+        </a>
+      </p>
+    </div>
+  ));
   return (
     <div>
-      <p className="text-main">Projects</p>
+      <div className="bg-mainSoftGray">
+        <p className="text-mainBlack text-left text-4xl sm:text-6xl">
+          Alex Williams
+        </p>
+
+        <div className="w-full mt-12 text-white bg-mainGunmetal border-mainDarkBlue border-4">
+          <div className="p-5">
+            <div>
+              <p className="text-lg sm:text-3xl lg:text-3xl">Projects</p>
+            </div>
+            <hr />
+            <div>{projectsDiv}</div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
